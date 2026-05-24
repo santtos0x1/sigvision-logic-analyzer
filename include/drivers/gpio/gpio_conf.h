efded0ab5936df33444e26c83cf23d1cdf2fa2ce
Santtos <ruanrodrigues393@gmail.com>
2026-05-24 00:56:08 -0300

    Feat: Adding GPIO configuration function

---
+++ include/drivers/gpio/gpio_conf.h
@@ -0,0 +1,3 @@
+#pragma once
+
+__attribute((always_inline)) inline void gpio_init(void);
---
+++ src/drivers/gpio/gpio_conf.c
@@ -0,0 +1,32 @@
+#include "drivers/gpio/gpio_conf.h"
+
+#include "stm32f4xx.h"
+
+__attribute((always_inline)) inline void gpio_init(void)
+{
+    // Enables GPIOA clock
+    RCC->AHB1ENR &= ~(RCC_AHB1ENR_GPIOAEN_Msk);
+    RCC->AHB1ENR |= RCC_AHB1ENR_GPIOAEN;
+
+    RCC->AHB1ENR &= ~(RCC_AHB1ENR_GPIOBEN_Msk);
+    RCC->AHB1ENR |= RCC_AHB1ENR_GPIOBEN;
+
+    // Sets GPIOA pin 0-7 to input mode (Channel probes)
+    GPIOA->MODER &= ~(
+        GPIO_MODER_MODE0_Msk | GPIO_MODER_MODE1_Msk | GPIO_MODER_MODE2_Msk |
+        GPIO_MODER_MODE3_Msk | GPIO_MODER_MODE4_Msk | GPIO_MODER_MODE5_Msk |
+        GPIO_MODER_MODE6_Msk | GPIO_MODER_MODE7_Msk
+    );
+    
+    // Sets GPIOB pin 0 to input mode (CLK)
+    GPIOB->MODER &= ~(GPIO_MODER_MODE0_Msk);
+
+    // Sets GPIOA pin 0-7 to no pull
+    GPIOA->PUPDR &= ~(
+        GPIO_PUPDR_PUPD0_Msk | GPIO_PUPDR_PUPD1_Msk | GPIO_PUPDR_PUPD2_Msk |
+        GPIO_PUPDR_PUPD3_Msk | GPIO_PUPDR_PUPD4_Msk | GPIO_PUPDR_PUPD5_Msk |
+        GPIO_PUPDR_PUPD6_Msk | GPIO_PUPDR_PUPD7_Msk
+    );
+
+    GPIOB->PUPDR &= ~(GPIO_PUPDR_PUPD0_Msk);
+}
