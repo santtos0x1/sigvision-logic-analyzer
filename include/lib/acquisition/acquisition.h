bd7293305d2de44734a5700951eaa34d121b006a
Santtos <ruanrodrigues393@gmail.com>
2026-06-05 16:53:36 -0300

    Refactor: Removing an extra probe for the clock

--- include/lib/acquisition/acquisition.h
+++ include/lib/acquisition/acquisition.h
@@ -4,19 +4,18 @@
 #include "lib/services/error/error.h"
 
 /**
- * @brief Fills a 16-bit integer buffer with data samples from all 8 channel probes and the CLK probe.
+ * @brief Fills a buffer with data samples from all 8 probe channels.
  *
- * Each entry of the buffer represents the simultaneous state of all 8 probes and the CLK probe:
- * each bit corresponds to one channel, where bit 8 represents the CLK probe, bit 7 represents
- * probe7, and bit 0 represents probe0.
+ * Each entry of the buffer represents the simultaneous state of the 8 probes,
+ * where each bit corresponds to a probe channel.
  *
  * Bit mapping within each entry:
  * @code
- *   Bit:   8     7       6       5       4       3       2       1       0
- *          clk   probe7  probe6  probe5  probe4  probe3  probe2  probe1  probe0
+ *   Bit:   7       6       5       4       3       2       1       0
+ *          probe7  probe6  probe5  probe4  probe3  probe2  probe1  probe0
  *
- *   Example: 0x112 = 0b100010010
- *            -> probe1 = 1, probe4 = 1, clk = 1, all others = 0
+ *   Example: 0x12 = 0b00010010
+ *            -> probe4 = 1, probe1 = 1, all others = 0
  * @endcode
  *
  * @param[out] buff     Buffer to be filled with probe samples.
@@ -24,4 +23,4 @@
  *
  * @return log_err_t    Error code indicating success or failure.
  */
-log_err_t sample_acq(uint16_t *buff, uint16_t buff_len);
+log_err_t sample_acq(uint8_t *buff, uint16_t buff_len);
--- lib/drivers/gpio/gpio.c
+++ lib/drivers/gpio/gpio.c
@@ -10,9 +10,6 @@ __attribute((always_inline)) inline void gpio_init(void)
     RCC->AHB1ENR &= ~(RCC_AHB1ENR_GPIOAEN_Msk);
     RCC->AHB1ENR |= RCC_AHB1ENR_GPIOAEN;
 
-    RCC->AHB1ENR &= ~(RCC_AHB1ENR_GPIOBEN_Msk);
-    RCC->AHB1ENR |= RCC_AHB1ENR_GPIOBEN;
-
     // Sets GPIOA pin 0-7 to input mode (Channel probes)
     GPIOA->MODER &= ~(
         GPIO_MODER_MODE0_Msk | GPIO_MODER_MODE1_Msk | GPIO_MODER_MODE2_Msk |
@@ -20,30 +17,22 @@ __attribute((always_inline)) inline void gpio_init(void)
         GPIO_MODER_MODE6_Msk | GPIO_MODER_MODE7_Msk
     );
     
-    // Sets GPIOB pin 0 to input mode (CLK)
-    GPIOB->MODER &= ~(GPIO_MODER_MODE0_Msk);
-
     // Sets GPIOA pin 0-7 to no pull
     GPIOA->PUPDR &= ~(
         GPIO_PUPDR_PUPD0_Msk | GPIO_PUPDR_PUPD1_Msk | GPIO_PUPDR_PUPD2_Msk |
         GPIO_PUPDR_PUPD3_Msk | GPIO_PUPDR_PUPD4_Msk | GPIO_PUPDR_PUPD5_Msk |
         GPIO_PUPDR_PUPD6_Msk | GPIO_PUPDR_PUPD7_Msk
     );
-
-    GPIOB->PUPDR &= ~(GPIO_PUPDR_PUPD0_Msk);
 }
 
-uint16_t probes_read(void)
+uint8_t probes_read(void)
 {
-    uint16_t sample_data = 0;
+    uint8_t sample_data = 0;
     
     // 00000...11111111 -> 0xFF
     // 01010...10010100 -> GPIOA->IDR 32-bit register
     // 00000...10010100 -> Filtered data
     sample_data = GPIOA->IDR & 0xFF;
 
-    // Gets 1 bit of the CLK pin and merges to sample_data
-    sample_data |= (GPIOB->IDR & 0x01) << 8;
-    
     return sample_data;
 }
