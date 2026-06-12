68d58e62fb432abd74ecb41b44ef5a79c4a3c337
Santtos <ruanrodrigues393@gmail.com>
2026-06-12 07:45:40 -0300

    Refactor: Improving configurations structure pt.3

--- lib/drivers/time/systick.c
+++ lib/drivers/time/systick.c
@@ -2,13 +2,19 @@
 
 #include "stm32f4xx_hal.h"
 
+#define CLKSOURCE_INTERNAL_CLK 0b01
+#define SYSTICK_ENABLE_COUNTER 0b01
+
+#define ARM_SYSTICK_CTRL_CLKSOURCE      (CLKSOURCE_INTERNAL_CLK << SysTick_CTRL_CLKSOURCE_Pos)
+#define ARM_SYSTICK_CTRL_ENABLE_COUNTER (SYSTICK_ENABLE_COUNTER << SysTick_CTRL_ENABLE_Pos)
+
 __attribute__((always_inline)) inline void systick_init(void)
 {
     // Set systick to use internal clock
     SysTick->CTRL &= ~(SysTick_CTRL_CLKSOURCE_Msk);
-    SysTick->CTRL |= (0b01 << SysTick_CTRL_CLKSOURCE_Pos);
+    SysTick->CTRL |= ARM_SYSTICK_CTRL_CLKSOURCE;
 
     // Enables counter
     SysTick->CTRL &= ~(SysTick_CTRL_ENABLE_Msk);
-    SysTick->CTRL |= (0b01 << SysTick_CTRL_ENABLE_Pos);
+    SysTick->CTRL |= ARM_SYSTICK_CTRL_ENABLE_COUNTER;
 }
